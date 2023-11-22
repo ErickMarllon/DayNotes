7777670010007edd037024be1c53ad46decfd3f4
ErickMarllon <erick.marllon@outlook.com.br>
2023-11-21 23:18:25 -0300

    feat: update service

--- frontend/src/components/services/api.js
+++ frontend/src/components/services/api.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3001',
+  baseURL:
+    'https://day-notes-ten.vercel.app/' ||
+    'https://day-notes-git-main-erickmarllon.vercel.app/' ||
+    'https://day-notes-83zjjbron-erickmarllon.vercel.app/',
 });
 
 export default api;
